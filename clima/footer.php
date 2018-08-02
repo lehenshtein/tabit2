aa1cdba72eaf8eefbaa0a3ff5a888fe1af5460e7
Oleksii <ostu@diceus.com>
2018-08-02 12:53:06 +0300

    edit

---
+++ clima/footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Graphy
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+
+		<?php get_sidebar( 'footer' ); ?>
+
+		<div class="site-bottom">
+
+			<div class="site-info">
+	some information will be here soon
+			</div><!-- .site-info -->
+
+		</div><!-- .site-bottom -->
+
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
